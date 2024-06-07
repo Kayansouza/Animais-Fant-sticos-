5fc7ff29408e4e51cf3c1bd54424b44c2e3de26c
Kayansouza <rick.kayan@gmail.com>
2024-06-07 14:49:52 -0300

    Alterações e adições de novas funcionabilidades no projeto, sendo adicionado aos poucos o javascript

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const animais = document.getElementById('animais')
-console.log(animais);
+const tabMenu = document.querySelectorAll('.js-tabmenu li');
+const tabContent = document.querySelectorAll('.js-tabcontent section');
 
-const gridSection = document.getElementsByClassName('grid-section');
+function activeTab(index) {
+    tabContent.forEach((section) => {
+        section.classList.remove('ativo');
+    });
 
-console.log(gridSection)
+    if (tabContent[index]) { // Verifica se o índice existe
+        tabContent[index].classList.add('ativo');
+    }
+}
+
+tabMenu.forEach((itemMenu, index) => {
+    itemMenu.addEventListener('click', () => {
+        activeTab(index);
+        tabMenu.forEach((item) => item.classList.remove('ativo')); // Remove 'ativo' dos menus
+        itemMenu.classList.add('ativo'); // Adiciona 'ativo' ao menu clicado
+    });
+});
