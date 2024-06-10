1bf241207b25aaa38a6ccc58196f0059e228b35e
Kayansouza <rick.kayan@gmail.com>
2024-06-10 20:23:00 -0300

    Atualizações, e adicções de novas funcionabilidade do projeto, a funcção tabMenu está ativa

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+function iniTabNav() { 
 const tabMenu = document.querySelectorAll('.js-tabmenu li');
 const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+if(tabMenu.length && tabContent.length){ 
+    tabContent[0].classList.add ('ativo')
 
 function activeTab(index) {
     tabContent.forEach((section) => {
@@ -18,3 +22,27 @@ tabMenu.forEach((itemMenu, index) => {
         itemMenu.classList.add('ativo'); // Adiciona 'ativo' ao menu clicado
     });
 });
+}
+
+}
+
+function initAccordion () {
+
+document.addEventListener('DOMContentLoaded', function() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const ativoClass ='ativo'
+    accordionList[0].classList.add('ativo');
+    accordionList[0].nextElementSibling.classList.add('ativo')
+  
+    function activeAccordion() {
+        this.classList.toggle('ativo')
+      this.nextElementSibling.classList.toggle('ativo');
+    }
+  
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
+    });
+  });
+}
+
+initAccordion()
